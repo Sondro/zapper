f41abb1deb559ecb774fb68325b83db88d437ea9
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2019-03-18 18:29:51 -0300

    Use ChatContext

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Routes from './../Routes'
 
+import ChatProvider from './../../context/Chat'
+
 const App = () => (
   <main>
-    <Routes />
+    <ChatProvider>
+      <Routes />
+    </ChatProvider>
   </main>
 )
 
