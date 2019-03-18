0013eeefc5db88a2cd76565311a2a1e2ba174111
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2019-03-18 18:01:35 -0300

    Improve App component to render Routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Routes from './../Routes'
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>App</div>
-    )
-  }
-}
+const App = () => (
+  <main>
+    <Routes />
+  </main>
+)
+
+export default App
