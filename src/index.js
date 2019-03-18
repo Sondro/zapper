7d039b30bf9276ce13e5a482c5f7d1796fa84cb7
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2019-03-18 17:46:14 -0300

    Fix App component path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './App'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
 const rootElement = document.getElementById('root')
